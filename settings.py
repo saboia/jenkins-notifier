bc8a1996ca4a0e966dfac4b5ebcd6113ec0fcd05
saboia <saboia@corp.globo.com>
2013-04-10 14:57:49 -0300

    removendo informacoes a mais

--- settings.py
+++ settings.py
@@ -3,7 +3,4 @@ import os
 #your jenkins url with port(if you have)
 JENKINS_URL = 'http://localhost:8080'
 
-#active jenkins job to validate deploy
-JOBS_TO_VALIDATE = [ 'Job1','Job2']
-
 PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
