43aca0ee4bdf807ffbe164321f8713d2c531b92b
saboia <saboia@corp.globo.com>
2013-04-10 14:34:27 -0300

    colocando a espera de 5 em 5 minuto

--- jenkins-notifier.py
+++ jenkins-notifier.py
@@ -6,22 +6,25 @@ from jenkinsapi.jenkins import Jenkins
 from jenkinsapi.api import get_latest_build
 
 import settings
+import time
 
 if __name__ == '__main__':
-    
-    je = Jenkins(settings.JENKINS_URL)
-    
-    jobs_list = je.get_jobs_list()
-    
-    jobs_broken = []
-    
-    for job_name in jobs_list:
+    while True:
+        je = Jenkins(settings.JENKINS_URL)
         
-        last_build = get_latest_build(settings.JENKINS_URL, job_name)
+        jobs_list = je.get_jobs_list()
         
-        if not last_build.is_good():
-            jobs_broken.append(job_name)
+        jobs_broken = []
+        
+        for job_name in jobs_list:
             
-    if jobs_broken:
-        p = GrowlPing()
-        p.notify(jobs_broken)
+            last_build = get_latest_build(settings.JENKINS_URL, job_name)
+            
+            if not last_build.is_good():
+                jobs_broken.append(job_name)
+                
+        if jobs_broken:
+            p = GrowlPing()
+            p.notify(jobs_broken)
+        
+        time.sleep(300)
